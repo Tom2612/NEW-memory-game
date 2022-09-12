30e3d58fd8a4e23de0c54ae88af3e43e5353405e
Tom <tomcat-26@hotmail.co.uk>
2022-09-12 13:32:20 +0100

    Copied code over to App.js and index.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import React from 'react';
+import Header from './components/Header';
+import Main from './components/Main';
+import Footer from './components/Footer';
+
 function App() {
   return (
-    <div className="App">
-      
+    <div className="body">
+      <Header />
+      <Main />
+      <Footer />
     </div>
-  );
+    
+  )
 }
 
 export default App;
