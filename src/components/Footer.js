ef5df772ae95bfe2e78039133a1b64039f9d3264
Tom <tomcat-26@hotmail.co.uk>
2022-09-12 13:35:11 +0100

    Added footer

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Footer() {
+    return (
+        <footer>
+            <h3><a href="https://github.com/Tom2612">Tom2612</a></h3>
+        </footer>
+    )
+}
